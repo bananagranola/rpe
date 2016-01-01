1d790219a92db2e3ec9e18888f828beee24ef0a3
atc <rainermoerlinghartheim@yahoo.com>
2016-01-01 11:34:31 -0800

    Initial deployment of this app to the web

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Ravelry Project Exporter',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+      description='Export Ravelry projects to CSV files',
+      author='bananagranola',
+      author_email='',
+      url='',
+      install_requires=['Flask', 'flask-wtf'],
      )
