f5c7b4b80bbad8b1ef9909490c99393dc8f9f59a
atc <rainermoerlinghartheim@yahoo.com>
2016-01-01 11:37:00 -0800

    Initial staging and deployment of this app to OpenShift

--- wsgi/run.py
+++ wsgi/run.py
@@ -1,3 +1,15 @@
-from app import app
+import os
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+#Create our index or root / route
+@app.route("/")
+@app.route("/index")
+def index():
+    return "This is the application mynote & I'm alive"
+
 if __name__ == "__main__":
-    app.run(debug = True) #We will set debug false in production 
+    app.run(debug = "True")
